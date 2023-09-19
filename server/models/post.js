6aa40d552885d97ea0b62a74919995cb616abaea
RamyaMandapati <ramya.mandapati@sjsu.edu>
2023-09-19 10:47:38 -0700

    post model

--- server/models/post.js
+++ server/models/post.js
@@ -1,87 +1,48 @@
-const Post = require("../models/post");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    user: {
+ type: mongoose.Schema.Types.ObjectId,
+ ref: 'user', // Reference to the User model for the post's author
+ required: true,
+},
+    tripCountry: {type: String},
+    tripState: {type: String},
+    locationName: {type: String},
+    fromDate:  {type: Date},
+    toDate:  {type: Date},
+    imageUrls: [{
+    type: String,  }],
+    Title: {type: String},
+    Description: {type: String},
+    itineraryID: {type: Integer},
+    likes: [{
+ type: mongoose.Schema.Types.ObjectId,
+ ref: 'user', // Reference to users who liked the post
+}],
+comments: [{
+ user: {
+   type: mongoose.Schema.Types.ObjectId,
+   ref: 'user', // Reference to the user who made the comment
+   required: true,
+ },
+ text: {
+   type: String,
+   required: true,
+ },
+ createdAt: {
+   type: Date,
+   default: Date.now,
+ },
+}],    
+tags: [{type: String},],
+createdAt: {
+ type: Date,
+ default: Date.now,
+},
+genderPref: {type: String},
+minAge : {type: Integer},
+maxAge: {type: Integer},
+});
 
-// Create a new post
-const addPost = async (req, res) => {
-  try {
-    const {
-      user,
-      tripCountry,
-      tripState,
-      locationName,
-      fromDate,
-      toDate,
-      imageUrls,
-      Title,
-      Description,
-      itineraryID,
-      tags,
-      genderPref,
-      minAge,
-      maxAge,
-    } = req.body;
-
-    const newPost = new Post({
-      user,
-      tripCountry,
-      tripState,
-      locationName,
-      fromDate,
-      toDate,
-      imageUrls,
-      Title,
-      Description,
-      itineraryID,
-      tags,
-      genderPref,
-      minAge,
-      maxAge,
-    });
-
-    const savedPost = await newPost.save();
-    res.status(201).json(savedPost);
-  } catch (error) {
-    console.error("Error creating a post:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-// Get all posts
-const getPosts = async (req, res) => {
-  try {
-    const posts = await Post.find();
-    res.json(posts);
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-// Filter posts based on criteria (e.g., trip type or status)
-const filterPosts = async (req, res) => {
-  try {
-    const { tripCountry, tripState, tags } = req.query;
-    const filter = {};
-
-    if (tripCountry) {
-      filter.tripCountry = tripCountry;
-    }
-    if (tripState) {
-      filter.tripState = tripState;
-    }
-    if (tags) {
-      filter.tags = { $in: tags.split(",") }; // Convert comma-separated tags to an array
-    }
-
-    const filteredPosts = await Post.find(filter);
-    res.json(filteredPosts);
-  } catch (error) {
-    console.error("Error filtering posts:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-module.exports = {
-  addPost,
-  getPosts,
-  filterPosts,
-};
+module.exports = mongoose.model('post', postSchema);
